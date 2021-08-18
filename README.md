36eedf0ae62101859e2f379dd3205fb8bcdbeae8
raosan <raosanf.lillahi@gmail.com>
2021-08-18 09:35:57 +0000

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Karmacap App
+
+Custom App to extend functionality of Karmacap erpnext
+
+#### License
+
+MIT
---
+++ karmacap_app/config/desktop.py
@@ -0,0 +1,12 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Karmacap App",
+			"color": "grey",
+			"icon": "octicon octicon-file-directory",
+			"type": "module",
+			"label": _("Karmacap App")
+		}
+	]
